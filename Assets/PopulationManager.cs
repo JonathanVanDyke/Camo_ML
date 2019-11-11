a910b1a6779416635bd809bfcaf664b0dc8f1880
Jonathan <jfv213@gmail.com>
2019-11-11 15:42:35 -0500

    Add genetic algo

--- Assets/PopulationManager.cs
+++ Assets/PopulationManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PopulationManager : MonoBehaviour
 {
@@ -32,6 +33,36 @@ public class PopulationManager : MonoBehaviour
       go.GetComponent<DNA>().b = Random.Range(0.0f, 1.0f);
       population.Add(go);
     }
+  }
+
+  GameObject Breed(GameObject parent1, GameObject parent2) {
+    Vector3 pos = new Vector3(Random.Range(-9,9),Random.Range(-4.5f,4.5f),0);
+    GameObject offspring = Instantiate(personPrefab, pos, Quaternion.identity);
+    DNA dna1 = parent1.GetComponent<DNA>();
+    DNA dna2 = parent1.GetComponent<DNA>();
+
+    // 50% of the time you get parent 1 dna or parent 2 dna
+    offspring.GetComponent<DNA>().r = Random.Range(0,10) < 5 ? dna1.r : dna2.r;
+    offspring.GetComponent<DNA>().g = Random.Range(0,10) < 5 ? dna1.g : dna2.g;
+    offspring.GetComponent<DNA>().b = Random.Range(0,10) < 5 ? dna1.b : dna2.b;
+    return offspring;
+  }
+
+  void BreedNewPopulation() {
+    List<GameObject> newPopulation = new List<GameObject>();
+    List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<DNA>().timeToDie).ToList();
+
+    population.Clear();
+    for (int i = (int) (sortedList.Count / 2.0f) - 1; i < sortedList.Count - 1; i++)
+    {
+      population.Add(Breed(sortedList[i], sortedList[i + 1]));
+      population.Add(Breed(sortedList[i + 1], sortedList[i]));
+    }
+
+    for (int i = 0; i < sortedList.Count; i++) {
+      Destroy(sortedList[i]);
+    }
+    generation++;
   }
 
   // Update is called once per frame
